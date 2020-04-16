937b630b87a0040b84d2265f870b69155238ac53
mmatti <mmatti@student.ethz.ch>
2020-04-16 14:51:21 +0200

    shorter try-catch block in plot-benchmark.py

--- benchmark/plot-benchmark.py
+++ benchmark/plot-benchmark.py
@@ -60,54 +60,54 @@ def simplecount(filename):
         lines += 1
     return lines
 
+filename = sys.argv[-1]
 try:
-    filename = sys.argv[-1]
     f = open(filename, 'r')
-    fileAsList = f.readlines()
-
-    num_lines = len(open(filename).readlines())
-    versions = find_versions(num_lines)
-
-    myDict = {} 
-
-    for version in versions:
-        myDict[version] = []
-
-    for i in range(num_lines):
-        line = fileAsList[i].split()
-        version = line[0]
-        n = int(line[1])
-        performance = float(line[4])
-        myDict[version].append((n,performance))
-
-    plot_setup()
-
-    for version in versions:
-        points = myDict[version]
-        x0 = [n for (n,p) in points]
-        y0 = [p for (n,p) in points]
-        plt.plot(x0,y0,'.-', linewidth=2,label=version)
-
-    plt.gcf().text(0.125, 0.9, "Performance [Flops/Cycle] vs. input size", fontsize=10)
-    title = "Performance Plot on" + get_processor_info()
-    plt.title(title,loc='left',y=1.06,fontsize=12)
-
-    ax = plt.gca()
-    ax.grid(which='major', axis='y')
-    plt.ylim((0, 20))
-    plt.yticks(fontsize=10)
-    plt.axes().xaxis.set_label_coords(0.5, -0.15)
-    plt.axes().tick_params(left= False)
-    plt.xscale('log',basex=2)
-    plt.xticks(fontsize=10, rotation=0)
-    plt.tick_params(
-        axis='x',          # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom=True,      # ticks along the bottom edge are off
-        top=False,         # ticks along the top edge are off
-        labelbottom=True) # labels along the bottom edge are off
-    plt.legend()
-    plt.savefig(filename + ".pdf")
-    plt.show()
 except:
     print(filename + " can not be opened")
+
+fileAsList = f.readlines()
+num_lines = len(open(filename).readlines())
+versions = find_versions(num_lines)
+
+myDict = {} 
+
+for version in versions:
+    myDict[version] = []
+
+for i in range(num_lines):
+    line = fileAsList[i].split()
+    version = line[0]
+    n = int(line[1])
+    performance = float(line[4])
+    myDict[version].append((n,performance))
+
+plot_setup()
+
+for version in versions:
+    points = myDict[version]
+    x0 = [n for (n,p) in points]
+    y0 = [p for (n,p) in points]
+    plt.plot(x0,y0,'.-', linewidth=2,label=version)
+
+plt.gcf().text(0.125, 0.9, "Performance [Flops/Cycle] vs. input size", fontsize=10)
+title = "Performance Plot on" + get_processor_info()
+plt.title(title,loc='left',y=1.06,fontsize=12)
+
+ax = plt.gca()
+ax.grid(which='major', axis='y')
+plt.ylim((0, 4))
+plt.yticks(fontsize=10)
+plt.axes().xaxis.set_label_coords(0.5, -0.15)
+plt.axes().tick_params(left= False)
+plt.xscale('log',basex=2)
+plt.xticks(fontsize=10, rotation=0)
+plt.tick_params(
+    axis='x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom=True,      # ticks along the bottom edge are off
+    top=False,         # ticks along the top edge are off
+    labelbottom=True) # labels along the bottom edge are off
+plt.legend()
+plt.savefig(filename + ".pdf")
+plt.show()
