cc299f92403c0e09228a9059f27274b97c11cb60
zurflusi <43852140+zurflusi@users.noreply.github.com>
2020-04-16 16:05:32 +0200

    fixed random seed to 0

--- src/main/social_force.c
+++ src/main/social_force.c
@@ -116,6 +116,7 @@ int main(int argc, char *argv[])
 */
 void initialize_people(double *position, double *desired_direction, double *final_destination, double *desired_speed, int n)
 {
+    srand(0);
     for (int i = 0; i < n; i++)
     {
         // initialize values independant of starting point and target point
